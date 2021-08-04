89ee4dc86bd49a222e5a9122abf9c80d0f6c219e
Joseph-K-B <303josephb@gmail.com>
2021-08-04 09:10:03 -0700

    recipe text

--- src/Ingredient-List.js
+++ src/Ingredient-List.js
@@ -7,7 +7,26 @@ class Ingredients extends Component {
     render() {
         return (
             <section>
-                <img src={list} alt='notepad'/>
+                <div id='notepad'>
+                <img src={list} alt='notepad'>
+                </img>
+                <ul id='left-list'>
+                    <li>1/2 cups milk</li>
+                    <li>1/1 cup mascarpone</li>
+                    <li>1/2 tsp pink salt</li>
+                    <li>1 lb Black Mission Figs</li>
+                    <li>1/2 cup brown sugar</li>
+                    <li>2-4 tbsp water</li>
+                </ul>
+                <ul id='right-list'>
+                    <li>1 1/2 cups heavy cream</li>
+                    <li>1/3 granulated sugar</li>
+                    <li>2 egg yolks</li>
+                    <li>1 lemon, juiced</li>
+                    <li>2 tbsp butter</li>
+                    <li>1 cup honey roasted pecans, roughly chopped</li>
+                </ul>
+                </div>
             </section>
         )
     }
